9951aadd1370b82892eff4f746447484812f609f
Karlo Verde <karlo.verde.1996@gmail.com>
2022-07-25 02:01:57 -0500

    feat(auth): self profile endpoint

--- back/server/api/v1/auth/router.js
+++ back/server/api/v1/auth/router.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { signUp, logIn } = require('./controller');
+const { me } = require('./middlewares');
+const { signUp, logIn, getProfile } = require('./controller');
 
 const router = express.Router();
 
-router.post('/sign-up', signUp).post('/log-in', logIn);
+router
+  .post('/sign-up', signUp)
+  .post('/log-in', logIn)
+  .get('/me', me, getProfile);
 
 module.exports = router;
