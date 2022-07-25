231af55d163694b56d848b8989739149a5423ea6
Karlo Verde <karlo.verde.1996@gmail.com>
2022-07-25 02:12:35 -0500

    chore(users): use env data for default admin

--- back/config.js
+++ back/config.js
@@ -2,6 +2,12 @@ require('dotenv').config();
 
 const config = {
   port: process.env.PORT,
+  defaultAdmin: {
+    name: process.env.DEFAULT_ADMIN_NAME,
+    lastName: process.env.DEFAULT_ADMIN_LAST_NAME,
+    email: process.env.DEFAULT_ADMIN_EMAIL,
+    password: process.env.DEFAULT_ADMIN_PASSWORD,
+  },
   database: {
     protocol: process.env.DB_PROTOCOL,
     url: process.env.DB_URL,
