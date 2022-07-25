3c2d22a020989589ceb21d68c22f5bd66f4fe92d
Karlo Verde <karlo.verde.1996@gmail.com>
2022-07-25 01:43:44 -0500

    chore(auth): login controller method

--- back/server/api/v1/auth/controller.js
+++ back/server/api/v1/auth/controller.js
@@ -1,3 +1,8 @@
+const { sign } = require('jsonwebtoken');
+const {
+  jwt: { secret: jwtSecret, expiresIn: jwtExpiresIn },
+} = require('../../../../config');
+const { UnauthorizedErrorResponse } = require('../../../responses');
 const { User } = require('../users/entity');
 
 exports.signUp = async (req, res, next) => {
@@ -8,5 +13,21 @@ exports.signUp = async (req, res, next) => {
     res.status(201).json(createdUserDoc);
   } catch (err) {
     next(err);
+  }
+};
+
+exports.logIn = async (req, res, next) => {
+  try {
+    const invalidCredsError = UnauthorizedErrorResponse('Invalid credentials');
+    const { email = '', password = '' } = req.body;
+    const userDoc = await User.findOne({ email });
+    if (!userDoc) return next(invalidCredsError);
+    const isPasswordValid = await userDoc.verifyPassword(password);
+    if (!isPasswordValid) return next(invalidCredsError);
+    const { _id: userId } = userDoc;
+    const jwt = sign({ id: userId }, jwtSecret, { expiresIn: jwtExpiresIn });
+    return res.status(200).json({ ...userDoc.toJSON(), jwt });
+  } catch (err) {
+    return next(err);
   }
 };
