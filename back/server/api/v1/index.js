5f13fcd3fdd0008d22a9d94900753206fc7fe5e3
Karlo Verde <karlo.verde.1996@gmail.com>
2022-07-30 00:23:32 -0500

    feat(products): expose products endpoints

--- back/server/api/v1/index.js
+++ back/server/api/v1/index.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.use('/auth', require('./auth/router'));
 router.use('/health', require('./health').router);
 router.use('/users', require('./users/router'));
+router.use('/products', require('./products/router'));
 
 // OpenAPI documentation
 router.use('/docs', require('./docs').router);
