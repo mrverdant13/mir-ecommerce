ddf0cb4a3326c1fe86d931e1d8248fd9b7a13363
Karlo Verde <karlo.verde.1996@gmail.com>
2022-07-30 05:11:49 -0500

    docs(products): add product photo

--- back/server/api/v1/products/docs.js
+++ back/server/api/v1/products/docs.js
@@ -85,6 +85,53 @@ exports.productsPaths = {
       },
     },
   },
+  '/products/{productId}/photo': {
+    post: {
+      tags: [this.productsTag.name],
+      summary: 'Add product photo',
+      description: 'Add product photo.',
+      operationId: '/products/productId/photo.post',
+      security: [{ Bearer: [] }],
+      parameters: [
+        {
+          name: 'productId',
+          in: 'path',
+          description: 'Product ID',
+          required: true,
+          schema: {
+            type: 'string',
+            format: 'objectID',
+          },
+        },
+      ],
+      requestBody: {
+        description: 'Product photo data.',
+        required: true,
+        content: {
+          'multipart/form-data': {
+            schema: {
+              type: 'object',
+              properties: {
+                photo: {
+                  type: 'string',
+                  format: 'binary',
+                  description: 'Product photo',
+                },
+              },
+              required: ['photo'],
+            },
+          },
+        },
+      },
+      responses: {
+        ...simpleCreatedResBodyDoc('Product photo added.'),
+        ...defaultUnauthorizedResBodyDoc,
+        ...defaultForbiddenResBodyDoc,
+        ...defaultBadRequestResBodyDoc,
+        ...fallbackInternalServerErrorResBodyDoc,
+      },
+    },
+  },
   '/products/groups': {
     get: {
       tags: [this.productsTag.name],
@@ -209,6 +256,13 @@ exports.productsSchemas = {
         minlength: 1,
         items: {
           type: 'string',
+        },
+      },
+      photos: {
+        type: 'array',
+        items: {
+          type: 'string',
+          format: 'path',
         },
       },
       createdAt: {
