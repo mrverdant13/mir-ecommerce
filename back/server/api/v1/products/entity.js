e8120dec8f4fe2c9299e3326ecda04e11bf147b8
Karlo Verde <karlo.verde.1996@gmail.com>
2022-07-30 00:21:09 -0500

    chore(products): create product entity

---
+++ back/server/api/v1/products/entity.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const fields = {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  groups: {
+    type: [String],
+    required: true,
+    minlength: 1,
+  },
+  categories: {
+    type: [String],
+    required: true,
+    minlength: 1,
+  },
+};
+
+const productSchema = new mongoose.Schema(fields, { timestamps: true });
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = {
+  Product,
+};
