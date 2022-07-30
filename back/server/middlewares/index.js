3fe155800f7a777b415a50c3114dfcea01a48970
Karlo Verde <karlo.verde.1996@gmail.com>
2022-07-30 05:04:01 -0500

    chore(back): add uploads middleware

--- back/server/middlewares/index.js
+++ back/server/middlewares/index.js
@@ -2,10 +2,12 @@ const reqIdSetter = require('./req-id-setter');
 const { paginator } = require('./paginator');
 const reqLogger = require('./logger');
 const errorHandlers = require('./error');
+const uploads = require('./uploads');
 
 module.exports = {
   reqIdSetter,
   reqLogger,
   paginator,
   errorHandlers,
+  uploads,
 };
