8e893fe7ee58c94ee0eaf1649d357b0bdef9db80
Karlo Verde <karlo.verde.1996@gmail.com>
2022-07-30 01:54:21 -0500

    feat(products): reatrieve categories by group

--- back/server/api/v1/products/router.js
+++ back/server/api/v1/products/router.js
@@ -6,6 +6,7 @@ const {
   listProducts,
   listProductGroups,
   updateGroupForAllProducts,
+  listProductCategoriesByGroup,
 } = require('./controller');
 
 const router = express.Router();
@@ -13,5 +14,6 @@ const router = express.Router();
 router.route('/').get(paginator, listProducts).post(me, isAdmin, createProduct);
 router.route('/groups').get(listProductGroups);
 router.route('/groups/:group').put(me, isAdmin, updateGroupForAllProducts);
+router.route('/groups/:group/categories').get(listProductCategoriesByGroup);
 
 module.exports = router;
