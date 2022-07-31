13723e4248ccaa70a887f425c58061dd8b1c5c6c
Karlo Verde <karlo.verde.1996@gmail.com>
2022-07-31 01:53:17 -0500

    chore(orders): create order product fields

---
+++ back/server/api/v1/orders/orderProduct.entity.js
@@ -0,0 +1,33 @@
+const orderProductFields = {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1,
+  },
+  photos: {
+    type: [String],
+    required: true,
+    default: [],
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+};
+
+module.exports = {
+  orderProductFields,
+};
