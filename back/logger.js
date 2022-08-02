ad877f64d04eac69d86383755999688e722bb480
Karlo Verde <karlo.verde.1996@gmail.com>
2022-08-02 02:09:11 -0500

    refactor(back): use logger on prod and dev only

--- back/logger.js
+++ back/logger.js
@@ -1,25 +1,32 @@
 const { createLogger, format, transports } = require('winston');
 
 // Transports
+const dummyTransport = new transports.Console({
+  level: 'emerg',
+});
+
 const errorFileTransport = new transports.File({
   format: format.json(),
   filename: 'logs/error.log',
   level: 'error',
 });
 
-const combinedConsoleTransport = new transports.Console({
+const allConsoleTransport = new transports.Console({
   format: format.combine(format.colorize({ all: true }), format.simple()),
   level: 'debug',
 });
 
 // Always bulk error logs to file.
-const logger = createLogger({
-  transports: [errorFileTransport],
-});
+const logger = createLogger({ transports: [dummyTransport] });
 
-// Log to console if not in production.
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(combinedConsoleTransport);
+// Log errors to file if in production.
+if (process.env.NODE_ENV === 'production') {
+  logger.add(errorFileTransport);
+}
+
+// Log to console if in development.
+if (process.env.NODE_ENV === 'development') {
+  logger.add(allConsoleTransport);
 }
 
 module.exports = logger;
