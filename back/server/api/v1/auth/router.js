429e8cfdf86839b0dd4a072797cfa2100bee7de9
Karlo Verde <karlo.verde.1996@gmail.com>
2022-08-05 20:32:10 -0500

    feat(auth): expose profile editor endpoint

--- back/server/api/v1/auth/router.js
+++ back/server/api/v1/auth/router.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { me } = require('./middlewares');
 const { signUp, logIn, getProfile } = require('./controller');
+const { editProfile } = require('../users/controller');
 
 const router = express.Router();
 
-router
-  .post('/sign-up', signUp)
-  .post('/log-in', logIn)
-  .get('/me', me, getProfile);
+router.post('/sign-up', signUp).post('/log-in', logIn);
+
+router.route('/me').all(me).get(getProfile).patch(editProfile);
 
 module.exports = router;
