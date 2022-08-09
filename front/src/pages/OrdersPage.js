94736e9af5492bfd897de9c8cb82e36dffe915b6
Karlo Verde <karlo.verde.1996@gmail.com>
2022-08-09 00:49:08 -0500

    feat(orders): display paginated orders

--- front/src/pages/OrdersPage.js
+++ front/src/pages/OrdersPage.js
@@ -1,25 +1,79 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 
-import { Box, Card, Typography } from '@mui/material';
+import { Box, Card, Divider, Typography } from '@mui/material';
 
 import AuthGuard from '../components/AuthGuard';
+import OrderTile from '../components/OrderTile';
+import { OrdersProvider, useOrdersContext } from '../context/orders';
 
 export default function OrdersPage() {
   return (
     <Box sx={{ p: { xs: 'auto', sm: 2 } }}>
       <AuthGuard>
-        <OrdersPageContent />
+        <OrdersProvider>
+          <OrdersPageContent />
+        </OrdersProvider>
       </AuthGuard>
     </Box>
   );
 }
 
 function OrdersPageContent() {
+  const { orders, hasMore, loading, error, loadOrders } = useOrdersContext();
+  const initialPageLoaded = useRef(false);
+  const onScroll = useCallback(
+    (e) => {
+      const scrollExtent = e.target.documentElement.scrollHeight;
+      const scrollPosition =
+        e.target.documentElement.scrollTop + window.innerHeight;
+      const threshold = 100;
+      const isBottom = scrollPosition + threshold >= scrollExtent;
+      const shouldLoadMore = isBottom && hasMore && !loading;
+      if (shouldLoadMore) loadOrders();
+    },
+    [hasMore, loading, loadOrders],
+  );
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [onScroll]);
+  useEffect(() => {
+    if (initialPageLoaded.current) return;
+    console.log('TEST');
+    loadOrders();
+    initialPageLoaded.current = true;
+  }, [loadOrders]);
+
   const content = (
     <>
       <Typography sx={{ mx: 2, my: 1 }} variant="h5">
         Orders
       </Typography>
+      <Divider />
+      {orders && (
+        <>
+          <Box sx={{ p: 2 }}>
+            {orders.map((order, index) => (
+              <React.Fragment key={order._id}>
+                {index !== 0 && <Divider sx={{ my: 2 }} />}
+                <OrderTile order={order} />
+              </React.Fragment>
+            ))}
+          </Box>
+        </>
+      )}
+      {loading && (
+        <Box sx={{ p: { xs: 'auto', sm: 2 } }}>
+          <Typography variant="h5" textAlign="center">
+            Loading
+          </Typography>
+        </Box>
+      )}
+      {error && (
+        <Typography variant="h5" textAlign="center">
+          {error}
+        </Typography>
+      )}
     </>
   );
 
